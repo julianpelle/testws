bbec5af9a8d8eef7a2942774f0420a3ffcfba16b
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-08-09 19:02:55 -0300

    Elimine archivos relacionados al User por falta de uso

--- src/main/java/com/academy/demo_ws/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.academy.demo_ws.repository;
-
-import com.academy.demo_ws.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
